dfb114dc58cce52f2726420c6cf2084ee9392076
Shobhit <shobhit9999999@gmail.com>
2023-09-20 18:59:00 +0530

    multi level inheritence a

---
+++ HybridINHT.java
@@ -0,0 +1,40 @@
+class Student{
+    String name = "Dev Verma";
+    int roll = 1010; 
+    void displayStudentInfo() {
+        System.out.println("Name :"+name);
+        System.out.println("Roll : "+roll);
+    }
+}
+class Marks extends Student 
+{
+    float sub1 = 33.0F;
+    float sub2 = 79.0F ;
+    void displayMarks(){
+        void displayStudentInfo() {
+            System.out.println("Subject 1 :"+sub1);
+            System.out.println("Subject 2 : "+sub2);
+            }
+    }
+    interface Sports {
+        
+        float sportMarks = 10.0F ;
+        void displaySportMarks();
+
+    }
+    class StudentResults extends Marks implements Sports 
+    {
+        float total = 0.0F ;
+        public void displaySportMarks(){
+            total = sportMarks + sub1 + sub2 ;
+            displayMarks();
+            System.out.println("total :"+total);
+        }
+    }
+}
+class HybridINHT {
+    public static void main (String a[]){
+        StudentResults obj1 = new StudentResults();
+        obj1.displaySportMarks();
+    }
+}
